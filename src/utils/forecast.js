b6590859afe26cfec69d2287fc9a014eceb86836
kamacoustic <75150502+kamacoustic@users.noreply.github.com>
2021-02-27 20:00:19 -0500

    Add feels like and humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -21,7 +21,7 @@ const forecast = (lon, lat, callback) => {
         } else if(body.error){
             callback('Unable to find location', undefined)
         } else{
-            callback(undefined, body.current.weather_descriptions[0] + `. It is currently ${body.current.temperature} degrees out. There is a ${body.current.precip}% chance of rain`)
+            callback(undefined, body.current.weather_descriptions[0] + `. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike}. With a humidity of ${body.current.humidity}%, There is a ${body.current.precip}% chance of rain.`)
         }
     })
 
